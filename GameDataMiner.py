1bba114160bc60d4a354be253dba2ad75d306124
Zachary Kornbluth <zkornbluth2007@gmail.com>
2020-01-13 11:06:18 -0600

    Added rush yards, ypc, explosive plays

--- GameDataMiner.py
+++ GameDataMiner.py
@@ -8,7 +8,7 @@ import sys
 def mainloop():
     args = sys.argv
     key = args[1]
-    
+
     params = get_params(key)
     teams = get_teams("Big Ten", params)
     games = get_games(teams, params)
@@ -42,7 +42,7 @@ def get_teams (name, params):
 # str, {str: str} -> listof str
 def get_games (opponents, params):
     r = requests.get('https://api.profootballfocus.com/v1/video/ncaa/games', headers = params)
-    
+
     games = []
     for game in r.json()['games']:
         if game['away_team'] in opponents and game['home_team'] in opponents:
@@ -59,7 +59,7 @@ def get_games (opponents, params):
 # For a list of games, return game level data for each
 # listof str, str -> {listof str}
 def game_level_data (games, params):
-    fields = ["Sacks"]
+    fields = ["Sacks", "Rush Yards", "Yards per Carry", "Explosive Plays"]
     header = ['Game ID', 'Winner', 'Loser']
     for field in fields:
         new_cols = ['Winner ' + field,
@@ -68,12 +68,15 @@ def game_level_data (games, params):
                     'Loser ' + field + ' Dif']
         header += new_cols
     results = [header]
-    
+
     for game in games:
         row = game
         r = requests.get('https://api.profootballfocus.com/v1/video/ncaa/games/'+game[0]+'/plays', headers = params)
         plays = r.json()['plays']
         row += count_sacks(plays, game[1], game[2])
+        row += count_rush_yards(plays, game[1], game[2])
+        row += get_ypc(plays, game[1], game[2])
+        row += count_explosive_plays(plays, game[1], game[2])
         results.append(row)
     return results
 
@@ -88,6 +91,54 @@ def count_sacks(plays, winner, loser):
             else:
                 sacks[1] = sacks[1] + 1
     return get_differentials(sacks)
+
+# For a list of plays and the winning team and losing team
+# count the rushing yards for each
+def count_rush_yards(plays, winner, loser):
+    rush_yards = [0, 0]
+    for play in plays:
+        if play['pff_RUNPASS'] == 'R':
+            if play['offense'] == winner:
+                rush_yards[0] = rush_yards[0] + play['pff_GAINLOSS']
+            else:
+                rush_yards[1] = rush_yards[1] + play['pff_GAINLOSS']
+    return get_differentials(rush_yards)
+
+# For a list of plays and the winning team and losing team
+# get the yards per carry for each
+def get_ypc(plays, winner, loser):
+    carries = [0, 0]
+    yards = [0, 0]
+    for play in plays:
+        if play['pff_RUNPASS'] == 'R':
+            if play['offense'] == winner:
+                carries[0] = carries[0] + 1
+                yards[0] = yards[0] + play['pff_GAINLOSS']
+            else:
+                carries[1] = carries[1] + 1
+                yards[1] = yards[1] + play['pff_GAINLOSS']
+    ypc = [yards[0]/carries[0], yards[1]/carries[1]]
+    return get_differentials(ypc)
+
+# For a list of plays and the winning team and losing team
+# count the number of explosive plays
+def count_explosive_plays(plays, winner, loser):
+    explosive_run = 10
+    explosive_pass = 20
+    explosive_plays = [0, 0]
+    for play in plays:
+        if play['pff_RUNPASS'] == 'R' and play['pff_GAINLOSS'] >= explosive_run:
+            if play['offense'] == winner:
+                explosive_plays[0] = explosive_plays[0] + 1
+            else:
+                explosive_plays[1] = explosive_plays[1] + 1
+        elif play['pff_RUNPASS'] == 'P' and play['pff_GAINLOSS'] >= explosive_pass:
+            if play['offense'] == winner:
+                explosive_plays[0] = explosive_plays[0] + 1
+            else:
+                explosive_plays[1] = explosive_plays[1] + 1
+    return get_differentials(explosive_plays)
+
 
 # For a list of two items, calculate
 # the first minus the second and the second minus the first
